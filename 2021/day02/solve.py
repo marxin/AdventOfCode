a554c17344afe0699852c1f50388c0811ae70a5d
marxin <mliska@suse.cz>
2021-12-02 06:56:21 +0100

    Finish part 2.

--- 2021/day02/solve.py
+++ 2021/day02/solve.py
@@ -6,7 +6,7 @@ folder = os.path.dirname(os.path.abspath(__file__))
 data = open(os.path.join(folder, 'input.txt')).read()
 lines = data.splitlines()
 
-position = [0, 0]
+position = [0, 0, 0]
 
 for line in lines:
     cmd, value = line.split()
@@ -14,10 +14,11 @@ for line in lines:
 
     if cmd == 'forward':
         position[0] += value
+        position[1] += value * position[2]
     elif cmd == 'down':
-        position[1] += value
+        position[2] += value
     elif cmd == 'up':
-        position[1] -= value
+        position[2] -= value
 
 print(position)
 print(position[0] * position[1])
