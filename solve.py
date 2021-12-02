b88b451f5f6704cfa1bcf4a1e75d69e2494cb011
marxin <mliska@suse.cz>
2021-12-02 07:01:07 +0100

    Improve template.

--- solve.py
+++ solve.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 
-data = open('input.txt').read()
+import os
+
+folder = os.path.dirname(os.path.abspath(__file__))
+data = open(os.path.join(folder, 'input.txt')).read()
 lines = data.splitlines()
 
 print(lines)
