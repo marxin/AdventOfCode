f905b666e736b87bcd3d10d7df7677bef88c3eda
marxin <mliska@suse.cz>
2021-12-06 07:15:59 +0100

    Finish day 5

--- 2021/day05/solve.py
+++ 2021/day05/solve.py
@@ -12,25 +12,21 @@ for line in lines:
     left, right = line.split(' -> ')
     x1, y1 = [int(x) for x in left.split(',')]
     x2, y2 = [int(x) for x in right.split(',')]
-    
-    if x1 == x2:
-        if y2 < y1:
-            y1, y2 = y2, y1
-        for y in range(y1, y2 + 1):
-            p = (x1, y)
-            if p in points:
-                points[p] += 1
-            else:
-                points[p] = 1
 
-    if y1 == y2:
-        if x2 < x1:
-            x1, x2 = x2, x1
-        for x in range(x1, x2 + 1):
-            p = (x, y1)
-            if p in points:
-                points[p] += 1
-            else:
-                points[p] = 1
+    diffX = x2 - x1
+    diffY = y2 - y1
+    diffXabs = abs(diffX)
+    diffYabs = abs(diffY)
+
+    if diffXabs == diffYabs or diffX == 0 or diffY == 0:
+        stepX = diffX / diffXabs if diffX else 0
+        stepY = diffY / diffYabs if diffY else 0
+
+    for i in range(max(diffXabs, diffYabs) + 1):
+        p = (x1 + i * stepX, y1 + i * stepY)
+        if p in points:
+            points[p] += 1
+        else:
+            points[p] = 1
 
 print(len([p for p in points.values() if p > 1]))
