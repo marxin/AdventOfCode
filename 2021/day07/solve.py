fad096a15e4b6cd06e93be0b867d0484421e8179
marxin <mliska@suse.cz>
2021-12-07 08:33:09 +0100

    Finish part 1.

--- 2021/day07/solve.py
+++ 2021/day07/solve.py
@@ -9,4 +9,13 @@ folder = os.path.dirname(os.path.abspath(__file__))
 data = open(os.path.join(folder, 'input.txt')).read()
 lines = data.splitlines()
 
-print(lines)
+values = [int(x) for x in lines[0].split(',')]
+
+best = sys.maxsize
+
+for position in range(max(values) + 1):
+    suma = sum(abs(x - position) for x in values)
+    if suma < best:
+        best = suma
+
+print(best)
