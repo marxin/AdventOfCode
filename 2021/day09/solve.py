ee613b43e65b8ce232a6c18ec811b8e5dd0e2b1e
marxin <mliska@suse.cz>
2021-12-09 12:20:49 +0100

    Finish part 2.

--- 2021/day09/solve.py
+++ 2021/day09/solve.py
@@ -4,6 +4,9 @@ import os
 import sys
 
 from collections import defaultdict, Counter
+from math import prod
+
+MOVES = ((1, 0), (0, 1), (-1, 0), (0, -1))
 
 folder = os.path.dirname(os.path.abspath(__file__))
 data = open(os.path.join(folder, 'input.txt')).read()
@@ -15,16 +18,35 @@ for y, line in enumerate(lines):
     for x, c in enumerate(line):
         data[(x, y)] = int(c)
 
-counter = 0
-print(len(data.items()))
+starts = []
+
 for key, value in data.items():
     done = True
-    for move in ((1, 0), (0, 1), (-1, 0), (0, -1)):
+    for move in MOVES:
         pos = (key[0] + move[0], key[1] + move[1])
         if pos in data:
             if data[pos] <= value:
                 done = False
     if done:
-        counter += value + 1
+        starts.append(key)
 
-print(counter)
+def get_bigger(key):
+    for move in MOVES:
+        pos = (key[0] + move[0], key[1] + move[1])
+        if pos in data and data[pos] > data[key] and data[pos] < 9:
+            yield pos
+
+def size_of_start(start):
+    seen = set([start])
+    stack = [start]
+
+    while stack:
+        p = stack.pop()
+        for bigger in get_bigger(p):
+            seen.add(bigger)
+            stack.append(bigger)
+
+    return len(seen)
+
+sizes = sorted([size_of_start(x) for x in starts], reverse=True)
+print(prod(sizes[:3]))
