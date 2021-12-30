2f001e26eb77e91e924130e6674f67a28bba7796
marxin <mliska@suse.cz>
2021-12-30 11:49:05 +0100

    Enhance template.

--- solve.py
+++ solve.py
@@ -3,10 +3,21 @@
 import os
 import sys
 
+from itertools import product, permutations
 from collections import defaultdict, Counter
+
+MOVES = ((1, 0), (0, -1), (-1, 0), (0, 1))
 
 folder = os.path.dirname(os.path.abspath(__file__))
 data = open(os.path.join(folder, 'input.txt')).read()
 lines = data.splitlines()
+
+data = {}
+H = len(lines)
+W = len(lines[0])
+
+for y, line in enumerate(lines):
+    for x, c in enumerate(line):
+        data[(x, y)] = c
 
 print(lines)
