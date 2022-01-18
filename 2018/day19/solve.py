0a5d9ca6280bc230cb0e93eb47407c3a584bc19a
marxin <mliska@suse.cz>
2022-01-18 21:48:17 +0100

    Finish part 2.

--- 2018/day19/solve.py
+++ 2018/day19/solve.py
@@ -36,40 +36,15 @@ regs[0] = 1
 
 counter = 0
 
-hack1 = 10551288
-
-progress = []
-last0 = 0
-
 while regs[ip] < len(instructions):
     counter += 1
     print(counter, regs)
-
-    # if regs == [0, 2, 9, 10551288, 1, 0]:
-        #regs[3] = 1000
-
-    
-
-    """
-    if regs == [1, 10, 9, 10551288, 100, 0]:
-        regs[4] = hack1 - 1
-    elif regs[1] == 10 and regs[2] == 9:
-        regs[1] = hack1 - 1
-    elif regs == [1, 100, 5, 10551288, 10551287, 0]:
-        regs[1] = hack1 - 1
-    """
-    
-    if counter > 1000:
-        if regs[0] != last0:
-            print(regs[0] - last0)
-            last0 = regs[0]
-
-    if regs == [1, 10, 9, 10551288, 100, 0]:
-        regs[4] = hack1 - 1
-
     parts = instructions[regs[ip]].split()
     ops = [None] + [int(x) for x in parts[1:]]
     regs[ops[3]] = opcodes[parts[0]](regs, ops)
     regs[ip] += 1
+
+    # Hack
+    # Wolfram: sum of all factors of 30481920
 
 print(regs[0])
