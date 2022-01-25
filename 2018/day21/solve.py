5412f2d27f9685de971da8957cdf6e8a57b24b43
marxin <mliska@suse.cz>
2022-01-25 23:47:19 +0100

    Finish part 1.

--- 2018/day21/solve.py
+++ 2018/day21/solve.py
@@ -3,7 +3,6 @@
 import os
 import sys
 
-regs = [0] * 6
 
 opcodes = {
         'addr': lambda regs, op: regs[op[1]] + regs[op[2]],
@@ -28,21 +27,48 @@ folder = os.path.dirname(os.path.abspath(__file__))
 data = open(os.path.join(folder, 'input.txt')).read()
 lines = data.splitlines()
 ip = int(lines[0].split()[-1])
-
 instructions = lines[1:]
 
-regs[0] = 0
+def run(value, limit, verbose):
+    regs = [0] * 6
+    regs[0] = value
+    counter = 0
 
-counter = 0
+    while regs[ip] < len(instructions):
+        counter += 1
+        insn = instructions[regs[ip]]
+        parts = insn.split()
+        ops = [None] + [int(x) for x in parts[1:]]
+        old_ip = regs[ip] + 1
 
-while regs[ip] < len(instructions):
-    counter += 1
-    insn = instructions[regs[ip]]
-    parts = insn.split()
-    ops = [None] + [int(x) for x in parts[1:]]
-    before = regs.copy()
-    regs[ops[3]] = opcodes[parts[0]](regs, ops)
-    regs[ip] += 1
-    print(f'ip(#{ip})={counter}', before, '->', regs, '//', insn)
+        if old_ip == 21 and regs[1] == 0:
+            lhs = regs[2] // 256
+            assert (lhs + 1) * 256 > regs[2]
+            if lhs * 256 > regs[2]:
+                lhs -= 1
+            regs[1] = lhs
+            regs[5] = (lhs + 1) * 256
+            counter += lhs * 7
 
-print(regs[0])
+        before = regs.copy()
+        regs[ops[3]] = opcodes[parts[0]](regs, ops)
+        regs[ip] += 1
+        if verbose:
+            print(f'counter={counter} ip(#{ip})={old_ip}', before, '->', regs, '//', insn)
+        if counter > limit:
+            return None
+        # assert old_ip != 29
+    print('exit after', counter)
+    return counter
+
+best = 11840402
+best_steps = 10e15
+
+for i in range(best, -1, -1):
+    assert i >= 0
+    r = run(i, best_steps, False)
+    print(i)
+    if r:
+        best = i
+        best_steps = r
+        print(best, best_steps)
