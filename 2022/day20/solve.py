ba7768b3a1c51ffbab50c084a5ca799466733b54
Martin Liska <mliska@suse.cz>
2022-12-20 17:37:21 +0100

    WIP for part 2 - don't have it

--- 2022/day20/solve.py
+++ 2022/day20/solve.py
@@ -10,6 +10,9 @@ from functools import reduce
 sys.setrecursionlimit(1000)
 
 MOVES = ((1, 0), (0, -1), (-1, 0), (0, 1))
+KEY = 0
+KEY = 811589153
+
 
 folder = os.path.dirname(os.path.abspath(__file__))
 input = open(os.path.join(folder, 'input.txt')).read()
@@ -21,15 +24,25 @@ numbers = []
 class Number:
     def __init__(self, n):
         self.n = n
+        self.wrap = n
 
 
 for y, line in enumerate(lines):
     n = int(line)
-    numbers.append(Number(n))
+    numbers.append(Number(n * KEY))
 
 LEN = len(numbers)
 assert len(set(numbers)) == LEN
 
+for n in numbers:
+    n.wrap = n.n % LEN
+    if n.n < 0:
+        n.wrap -= LEN
+    elif n.n > LEN:
+        n.wrap += LEN
+        print(n.n, n.wrap)
+
+print([(x.n, x.wrap) for x in numbers])
 
 def find_num(numbers, num):
     for i in range(LEN):
@@ -39,26 +52,35 @@ def find_num(numbers, num):
 
 
 def norm(x):
-    while x >= LEN:
-        x -= LEN
-    while x <= -LEN:
-        x += LEN
-    return x
+    return x % LEN
+
+
+def norm2(x):
+    y = x % LEN
+    if x < 0:
+        y -= LEN
+    if x >= LEN:
+        y += LEN
+    return y
+
 
 candidates = numbers.copy()
-for j, n in enumerate(candidates):
-    print(j)
-    i = find_num(numbers, n)
-    i2 = i + n.n
+print([x.n for x in numbers])
 
-    if i2 > i:
-        for j in range(i, i2):
-            numbers[norm(j)] = numbers[norm(j + 1)]
-    else:
-        for j in range(i, i2, -1):
-            numbers[norm(j)] = numbers[norm(j - 1)]
-    numbers[norm(i2)] = n
-    # print([x.n for x in numbers])
+for k in range(1):
+    for j, n in enumerate(candidates):
+        print(k, j)
+        i = find_num(numbers, n)
+        i2 = i + n.wrap
+
+        if i2 > i:
+            for j in range(i, i2):
+                numbers[norm(j)] = numbers[norm(j + 1)]
+        else:
+            for j in range(i, i2, -1):
+                numbers[norm(j)] = numbers[norm(j - 1)]
+        numbers[norm(i2)] = n
+        print([x.n for x in numbers])
 
 index = 0
 while True:
@@ -73,6 +95,7 @@ print('zero index', index)
 total = 0
 for i in (1000, 2000, 3000):
     i2 = index + i
+    print(numbers[norm(i2)].n)
 
     total += numbers[norm(i2)].n
 
