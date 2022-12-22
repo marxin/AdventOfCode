120db4b6cdd1c3883a6bcf9cd813ec285254aeb8
Martin Liska <mliska@suse.cz>
2022-12-22 23:18:50 +0100

    Finish part 2 of day 22.

--- 2022/day22/solve.py
+++ 2022/day22/solve.py
@@ -29,27 +29,6 @@ for y, line in enumerate(lines):
         if c in ('.', '#'):
             map[(x, y)] = c
 
-teleport = dict([(m, {}) for m in MOVES])
-
-for p in map:
-    for move in MOVES:
-        p2 = (p[0] + move[0], p[1] + move[1])
-        if p2 not in map:
-            match move:
-                case (1, 0):
-                    t = min([x for x in map if x[1] == p[1]])
-                case (-1, 0):
-                    t = max([x for x in map if x[1] == p[1]])
-                case (0, 1):
-                    t = min([x for x in map if x[0] == p[0]])
-                case (0, -1):
-                    t = max([x for x in map if x[0] == p[0]])
-            teleport[move][p2] = t
-
-start = min([p for p in map if p[1] == 0])
-orientation = 0
-
-
 def parse_commands(line):
     while line:
         cmd = ''.join(takewhile(lambda x: x not in ('L', 'R'), line))
@@ -59,28 +38,124 @@ def parse_commands(line):
 
 
 commands = list(parse_commands(second))
-print(start)
 
-for i, move in enumerate(commands):    
+N = 50
+W = 3
+H = 4
+assert len(map) == 6 * (N * N)
+
+
+class Face:
+    def __init__(self, x, y, map):
+        self.x = x
+        self.y = y
+        self.nr = y * W + x + 1
+        self.map = {}
+
+        for p, v in map.items():
+            if p[0] // N == self.x and p[1] // N == self.y:
+                self.map[(p[0] % N, p[1] % N)] = v
+
+    def __repr__(self):
+        return f'[Face(#{self.nr})]'
+
+
+def printmap(map):
+    for y in range(N):
+        for x in range(N):
+            print(map[(x, y)], end='')
+        print()
+    print()
+
+
+faces = {}
+for y in range(H):
+    for x in range(W):
+        f = Face(x, y, map)
+        if len(f.map) == N * N:
+            faces[f.nr] = f
+
+print(faces)
+
+
+def rotate_right(p):
+    assert 0 <= p[0] < N
+    assert 0 <= p[1] < N
+    return (N - p[1] - 1, p[0])
+
+
+def rotate_right_n(p, n):
+    for _ in range(n):
+        p = rotate_right(p)
+    return p
+
+
+# fill up neighbors
+faces[2].neighbors = ( (3, 0), (5, 0), (7, 2), (10, 3) )
+faces[3].neighbors = ( (8, 2), (5, 3), (2, 0), (10, 0) )
+faces[5].neighbors = ( (3, 1), (8, 0), (7, 1), (2, 0) )
+faces[7].neighbors = ( (8, 0), (10, 0), (2, 2), (5, 3) )
+faces[8].neighbors = ( (3, 2), (10, 3), (7, 0), (5, 0) )
+faces[10].neighbors = ( (8, 1), (3, 0), (2, 1), (7, 0) )
+
+# printmap(faces[2].map)
+
+x = (0, 0)
+x = rotate_right(x)
+assert x == (49, 0)
+x = rotate_right(x)
+assert x == (49, 49)
+x = rotate_right(x)
+assert x == (0, 49)
+x = rotate_right(x)
+assert x == (0, 0)
+assert rotate_right((2, 1)) == (48, 2)
+
+orientation = 0
+start = (0, 0)
+face = faces[2]
+
+# discard it now
+map = None
+
+for i, move in enumerate(commands):
+    assert 0 <= start[0] < N
+    assert 0 <= start[1] < N
+
     steps, rotate = move
-    print('Before', i, start, MOVES[orientation], 'steps:', steps)
+    print('Before', i, start, MOVES[orientation], move, 'steps:', steps)
 
     for _ in range(steps):
         move = MOVES[orientation]
         start2 = (start[0] + move[0], start[1] + move[1])
-        if start2 not in map:
-            t = teleport[move][start2]
-            if map[t] == '.':
-                start2 = t
+        if start2 not in face.map:
+            match orientation:
+                case 0:
+                    start2 = (0, start2[1])
+                case 1:
+                    start2 = (start2[0], 0)
+                case 2:
+                    start2 = (N - 1, start2[1])
+                case 3:
+                    start2 = (start2[0], N - 1)
+            
+            newface, newo = face.neighbors[orientation]
+            torot = (4 - newo) % len(MOVES)
+            start2 = rotate_right_n(start2, torot)
+            face2 = faces[newface]
+            if face2.map[start2] == '.':
+                start = start2
+                face = face2
+                orientation = (orientation + torot) % len(MOVES)
             else:
-                # can't teleport
+                # end the steps
                 break
-
-        if map[start2] == '.':
-            start = start2
         else:
-            # end the steps
-            break
+            if face.map[start2] == '.':
+                start = start2
+            else:
+                # end the steps
+                break
 
     # rotate
     match rotate:
@@ -92,6 +167,9 @@ for i, move in enumerate(commands):
             pass
     orientation %= len(MOVES) 
 
-print(start, orientation)
-p1 = 1000 * (start[1] + 1) + 4 * (start[0] + 1) + orientation
-print(p1)
+print(start)
+start = (start[0] + face.x * N, start[1] + face.y * N)
+print(start)
+
+p2 = 1000 * (start[1] + 1) + 4 * (start[0] + 1) + orientation
+print(p2)
