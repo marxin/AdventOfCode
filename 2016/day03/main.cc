6b773f7ed78426b5e97253eb3897a15fbf9ddb50
Martin Liska <mliska@suse.cz>
2023-02-03 22:31:21 +0100

    Finish part 2.

--- 2016/day03/main.cc
+++ 2016/day03/main.cc
@@ -3,12 +3,20 @@
 #include <unordered_set>
 #include <fstream>
 #include <sstream>
+#include <vector>
+#include <tuple>
 
 using namespace std;
 
 int main(int argc, char **argv)
 {
     unsigned int triangles = 0;
+    vector<vector<unsigned int>> values;
+
+    auto is_triangle = [](unsigned int a, unsigned int b, unsigned int c)
+    {
+        return (a + b > c && a + c > b && b + c > a);
+    };
 
     ifstream ifile("input.txt");
     for (string line; getline(ifile, line);)
@@ -17,12 +25,23 @@ int main(int argc, char **argv)
 
         stringstream ss{line};
         ss >> a >> b >> c;
+        values.push_back({a, b, c});
 
-        if ((a + b) > c && (a + c) > b && (b + c) > a)
+        if(is_triangle(a, b, c))
             triangles++;
     }
 
-    cout << triangles << endl;
+    cout << "part 1: " << triangles << endl;
+
+    // Part 2
+    unsigned int triangles2 = 0;
+
+    for (unsigned int c = 0; c < 3; c++)
+        for (unsigned i = 0; i < values.size() / 3; i++)
+            if(is_triangle(values[3 * i][c], values[3 * i + 1][c], values[3 * i + 2][c]))
+                triangles2++;
+
+    cout << "part 2: " << triangles2 << endl;
 
     return 0;
 }
