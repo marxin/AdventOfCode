91f3307fa49264fc084b97f2c1919fd498b7370f
Martin Liska <martin.liska@melowntech.com>
2023-10-01 20:45:04 +0200

    finish part 2

--- 2016/day07/src/main.rs
+++ 2016/day07/src/main.rs
@@ -1,11 +1,12 @@
-use std::fs;
+use std::{fs, collections::HashSet};
 
-fn supports_tls(packet: &str) -> bool {
-    let mut in_mask = false;
-    let mut found = false;
+fn supports_ssl(packet: &str) -> bool {
     let data: Vec<char> = packet.chars().collect();
+    let mut outside = HashSet::new();
+    let mut inside = HashSet::new();
+    let mut in_mask = false;
 
-    for index in 0..data.len() - 3 {
+    for index in 0..data.len() - 2 {
         let c = data[index];
         match c {
             '[' => {in_mask = true; continue; },
@@ -13,30 +14,27 @@ fn supports_tls(packet: &str) -> bool {
             _ => {}
         }
 
-        if data[index] == data[index + 3] &&
-            data[index + 1] == data[index + 2] &&
-            data[index] != data[index + 1] {
-            if in_mask {
-                return false;
+        if data[index] == data[index + 2] && data[index] != data[index + 1] {
+            if !in_mask {
+                inside.insert(vec![data[index], data[index + 1], data[index]]);
             } else {
-                found = true;
+                outside.insert(vec![data[index + 1], data[index], data[index + 1]]);
             }
         }
     }
 
-    found
+    inside.intersection(&outside).count() > 0
 }
 
 fn main() {
-    assert_eq!(supports_tls("abba[mnop]qrst"), true);
-    assert_eq!(supports_tls("abcd[bddb]xyyx"), false);
-    assert_eq!(supports_tls("aaaa[qwer]tyui"), false);
-    assert_eq!(supports_tls("ioxxoj[asdfgh]zxcvbn"), true);
+    assert_eq!(supports_ssl("aba[bab]xyz"), true);
+    assert_eq!(supports_ssl("xyx[xyx]xyx"), false);
+    assert_eq!(supports_ssl("aaa[kek]eke"), true);
+    assert_eq!(supports_ssl("zazbz[bzb]cdb"), true);
 
     let mut counter = 0;
-
     for line in fs::read_to_string("input.txt").unwrap().lines() {
-        if supports_tls(line) {
+        if supports_ssl(line) {
             counter += 1;
         }
     }
