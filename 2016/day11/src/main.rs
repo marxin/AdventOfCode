4c6053d36f0c9c84d1399538ba0778d5e88c6006
Martin Liska <martin.liska@melowntech.com>
2023-10-09 20:58:20 +0200

    finish part 1

--- 2016/day11/src/main.rs
+++ 2016/day11/src/main.rs
@@ -1,7 +1,7 @@
-use std::{fs, collections::HashMap, hash::Hash, hash::Hasher, ops::RangeFull};
+use std::{fs, collections::HashMap, hash::Hash};
 
 const FLOORS: usize = 4;
-const TYPES: usize = 2;
+const TYPES: usize = 5;
 const CHANGES: [i32; 2] = [-1, 1];
 
 #[derive(Debug, Clone, Copy)]
@@ -12,7 +12,6 @@ enum Candidate {
 
 #[derive(Debug, Hash, PartialEq, Eq, Clone)]
 struct Map {
-    steps: usize,
     elevator: usize,
     microchips: Vec<usize>,
     generators: Vec<usize>
@@ -20,11 +19,15 @@ struct Map {
 
 impl Map {
     fn new() -> Map {
-        Map {steps: 0, elevator: 0, microchips: vec![usize::MAX; TYPES], generators: vec![usize::MAX; TYPES] }
+        Map { elevator: 0, microchips: vec![usize::MAX; TYPES], generators: vec![usize::MAX; TYPES] }
+    }
+
+    fn end() -> Map {
+        Map { elevator: FLOORS - 1, microchips: vec![FLOORS - 1; TYPES], generators: vec![FLOORS - 1; TYPES] }
     }
 
     fn print(&self, short_namming: &Vec<char>) {
-        println!("=== steps: {} ===", self.steps);
+        println!("===");
         for floor in (0..FLOORS).rev() {
             print!("F{} ", floor + 1);
             if self.elevator == floor {
@@ -47,6 +50,7 @@ impl Map {
             }
             println!();
         }
+        println!();
     }
 
     fn get_candidates(&self) -> Vec<Candidate> {
@@ -77,14 +81,28 @@ impl Map {
         subsets
     }
 
-    fn try_all_possibilites(&self, short_namming: &Vec<char>) {
+    fn try_all_possibilites(&self, steps: usize, memory: & mut HashMap::<Map, usize>, short_namming: &Vec<char>) {
+        if steps > 50 {
+            return;
+        }
+        if memory.contains_key(self) {
+            let value = memory.get_mut(self).unwrap();
+            if steps >= *value {
+                return;
+            }
+            // update new minimum for the map
+            *value = steps;
+        }
+        else {
+            memory.insert(self.clone(), steps);
+        }
+
         for candidate in self.get_candidate_subsets() {
             for change in CHANGES {
                 let newfloor = (self.elevator as i32) + change;
                 if newfloor >= 0 && newfloor < FLOORS as i32 {
                     let mut next = self.clone();
                     next.elevator = newfloor as usize;
-                    next.steps += 1;
                     for c in candidate.clone() {
                         match c {
                             Candidate::Generator(i) => {
@@ -95,11 +113,29 @@ impl Map {
                             }
                         }
                     }
-                    println!();
-                    next.print(short_namming);
+                    // next.print(short_namming);
+                    if next.is_valid() {
+                        next.try_all_possibilites(steps + 1, memory, short_namming);
+                    }
                 }
             }            
         }
+    }
+
+    fn is_valid(&self) -> bool {
+        for floor in 0..FLOORS {
+            let generators: Vec<_> = self.generators.iter().enumerate().filter(|t| *t.1 == floor).map(|t| t.0).collect();
+            let microchips: Vec<_> = self.microchips.iter().enumerate().filter(|t| *t.1 == floor).map(|t| t.0).collect();
+            if !generators.is_empty() {
+                for m in microchips {
+                    if !generators.contains(&m) {
+                        return false;
+                    }
+                }
+            }
+        }
+
+        true
     }
 }
 
@@ -117,7 +153,7 @@ fn main() {
                 break;
             }
             match tokens[2] {
-                "microchip" => {
+                "microchip" | "microchip," => {
                     let mut mineral = tokens[1];
                     mineral = mineral.split('-').next().unwrap();
                     if !mineral_mapping.contains_key(mineral) {
@@ -125,26 +161,33 @@ fn main() {
                         short_namming.push(mineral.to_uppercase().chars().next().unwrap());
                     }
                     map.microchips[*mineral_mapping.get(mineral).unwrap()] = i;
-                    tokens = tokens.into_iter().skip(4).collect();
+                    tokens = tokens.into_iter().skip(3).collect();
                 }
-                "generator" => {
+                "generator" | "generator," => {
                     let mineral = tokens[1];
                     if !mineral_mapping.contains_key(mineral) {
                         mineral_mapping.insert(mineral, mineral_mapping.len());
                         short_namming.push(mineral.to_uppercase().chars().next().unwrap());
                     }
                     map.generators[*mineral_mapping.get(mineral).unwrap()] = i;
-                    tokens = tokens.into_iter().skip(4).collect();
+                    tokens = tokens.into_iter().skip(3).collect();
                 }
                 _ => {
                     panic!("unknown token");
                 }
             }
-        }        
+        }
     }
 
     println!("mapping = {:?}", short_namming);
     map.print(&short_namming);
     println!("");
-    map.try_all_possibilites(&short_namming);
+
+    assert_eq!(Map::new(), Map::new());
+
+    let mut memory = HashMap::new();
+    map.try_all_possibilites(0, & mut memory,&short_namming);
+
+    println!("memory size={}", memory.len());
+    println!("{}", memory.get(&Map::end()).unwrap());
 }
