56a24e6d55d62bb6915aded48875346a51416bdc
Martin Liska <martin.liska@melowntech.com>
2023-10-16 16:34:37 +0200

    finish part 1

---
+++ 2016/day18/src/main.rs
@@ -0,0 +1,28 @@
+fn get_next_row(input: Vec<char>) -> Vec<char> {
+    let mut result = Vec::new();
+
+    for (i, &center) in input.iter().enumerate() {
+        let left = if i == 0 { '.' } else { input[i - 1] };
+        let right = if i + 1 == input.len() { '.' } else { input[i + 1] };
+        let r = match (left, center, right ) {
+            ('^', '^', '.') | ('.', '^', '^' ) | ('^', '.', '.') | ('.', '.', '^') => { '^' }
+            _ => { '.' }
+        };
+        result.push(r);
+    }
+
+    result
+}
+
+fn main() {
+    let input_str = ".^^^.^.^^^^^..^^^..^..^..^^..^.^.^.^^.^^....^.^...^.^^.^^.^^..^^..^.^..^^^.^^...^...^^....^^.^^^^^^^";
+    let mut input: Vec<_> = input_str.clone().chars().collect();
+    let mut total = 0;
+
+    for _ in 0..40 {
+        total += input.iter().filter(|&&c| c == '.').count();
+        input = get_next_row(input);
+    }
+
+    println!("{total}");
+}
