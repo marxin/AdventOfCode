f0603ed6b0c8da2b2442b6acf108e96e274674fe
Martin Liska <martin.liska@hey.com>
2023-11-23 17:22:03 +0100

    Finish part 2

--- 2016/day22/src/main.rs
+++ 2016/day22/src/main.rs
@@ -2,8 +2,8 @@ use std::{fs, collections::{HashMap, VecDeque, HashSet}};
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 struct Disk {
-    x: usize,
-    y: usize,
+    x: i32,
+    y: i32,
     size: usize,
     used: usize,
     available: usize,
@@ -15,11 +15,11 @@ impl Disk {
         assert_eq!(parts.len(), 5);
         let coords: Vec<_> = parts[0].split('-').collect();
         assert_eq!(coords.len(), 3);
-        let x: usize = coords[1].strip_prefix('x').unwrap().parse().unwrap();
-        let y: usize = coords[2].strip_prefix('y').unwrap().parse().unwrap();
-        let size: usize = parts[1].strip_suffix('T').unwrap().parse().unwrap();
-        let used: usize = parts[2].strip_suffix('T').unwrap().parse().unwrap();
-        let available: usize = parts[3].strip_suffix('T').unwrap().parse().unwrap();
+        let x = coords[1].strip_prefix('x').unwrap().parse().unwrap();
+        let y = coords[2].strip_prefix('y').unwrap().parse().unwrap();
+        let size = parts[1].strip_suffix('T').unwrap().parse().unwrap();
+        let used = parts[2].strip_suffix('T').unwrap().parse().unwrap();
+        let available = parts[3].strip_suffix('T').unwrap().parse().unwrap();
         assert!(used <= size);
 
         Disk { x, y, size, used, available }
@@ -29,14 +29,22 @@ impl Disk {
 }
 
 const MOVES: [(i32, i32); 4] = [(1, 0), (0, 1), (-1, 0), (0, -1)];
+const MAX: i32 = 30;
 
 fn main() {
+    // All nodes are connected and we have all nodes in range [0-30]x[0-30].
     let mut disks = HashMap::new();
 
     for line in fs::read_to_string("input.txt").unwrap().lines().skip(2) {
         let disk = Disk::new(line);
         disks.insert((disk.x, disk.y), disk);
     }
+
+    let place = disks.values().find(|p| p.used == 0).unwrap();
+    assert_eq!(place.used, 0);
+    let fit = place.available;
+    println!("Space: {place:?}");
+    println!("Fit: {fit}");
 
     let mut viable = 0;
 
@@ -49,5 +57,30 @@ fn main() {
         }
     }
 
-    println!("{viable}");
+    println!("Viable: {viable}");
+
+    for y in 0..=MAX {
+        for x in 0..=MAX {
+            let disk = disks.get(&(x, y)).unwrap();
+            let c = {
+                if x == 0 && y == 0 {
+                    'S'
+                }
+                else if x == MAX && y == 0 {
+                    'G'
+                }
+                else if disk.used == 0 {
+                    '_'
+                } else if disk.used > fit {
+                    '#'
+                } else {
+                    '.'
+                }
+            };
+            print!("{c}");
+        }
+        println!();
+    }
+
+    println!("Part 2: {}", 9+27+25+29*5+1);
 }
