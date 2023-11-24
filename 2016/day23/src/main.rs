0a25eebc7ba5a89ad57352f1e56855bd3ddb4e02
Martin Liska <martin.liska@hey.com>
2023-11-24 09:50:35 +0100

    Finish part 1.

--- 2016/day23/src/main.rs
+++ 2016/day23/src/main.rs
@@ -9,7 +9,7 @@ struct Cpu {
 
 impl Cpu {
     fn new() -> Cpu {
-        Cpu {pc: 0, registers: HashMap::from([(String::from("a"), 0), (String::from("b"), 0), (String::from("c"), 0), (String::from("d"), 0)]), instructions: vec![]}
+        Cpu {pc: 0, registers: HashMap::from([(String::from("a"), 7), (String::from("b"), 0), (String::from("c"), 0), (String::from("d"), 0)]), instructions: vec![]}
     }
 
     fn get_value(&self, item: &String) -> i32 {
@@ -26,7 +26,14 @@ impl Cpu {
     fn run(& mut self) {
         while self.pc >= 0 && self.pc < self.instructions.len() as i32 {
             let insn = &self.instructions[self.pc as usize];
-            // println!("pc: {}, insn: {insn:?} {:?}", self.pc, self.registers);
+            /*
+            println!("pc: {}, insn: {insn:?} {:?}", self.pc, self.registers);
+            for insn in &self.instructions {
+                println!("{insn:?}");
+            }
+            println!();
+            */
+
             match insn[0].as_str() {
                 "cpy" => {
                     let value = self.get_value(&insn[1]);
@@ -41,8 +48,27 @@ impl Cpu {
                 "jnz" => {
                     let value = self.get_value(&insn[1]);
                     if value != 0 {
-                        self.pc += insn[2].parse::<i32>().unwrap();
+                        self.pc += self.get_value(&insn[2]);
                         continue;
+                    }
+                }
+                "tgl" => {
+                    let value = self.get_value(&insn[1]);
+                    let insn_index = self.pc + value;
+                    if insn_index >= 0 {
+                        let insn_index = insn_index as usize;
+                        if insn_index < self.instructions.len() {
+                            let replacement = match self.instructions[insn_index][0].as_str() {
+                                "inc" => "dec",
+                                "dec" => "inc",
+                                "jnz" => "cpy",
+                                "cpy" => "jnz",
+                                "tgl" => "inc",
+                                _ => panic!(),
+                            };
+                            self.instructions[insn_index][0] = replacement.to_string();
+                            println!("change instruction {insn_index} to {replacement}");
+                        }
                     }
                 }
                 _ => { panic!(); }
