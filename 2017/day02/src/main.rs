90d746b4bf441c63420012e4f191d921a6d71c58
Martin Liska <martin.liska@hey.com>
2023-11-25 15:56:36 +0100

    Finish part 2

--- 2017/day02/src/main.rs
+++ 2017/day02/src/main.rs
@@ -6,15 +6,29 @@ use itertools::{Itertools, MinMaxResult};
 #[allow(dead_code)]
 const MOVES: [(i32, i32); 4] = [(0, 1), (1, 0), (0, -1), (-1, 0)];
 
+fn dividable_result(values: &Vec<u32>) -> u32 {
+    values
+        .iter()
+        .cartesian_product(values)
+        .filter_map(|(&x, &y)| {
+            if x != y && x % y == 0 {
+                Some(x / y)
+            } else {
+                None
+            }
+        })
+        .next()
+        .unwrap()
+}
+
 fn main() {
     let mut sum = 0;
     for line in fs::read_to_string("input.txt").unwrap().lines() {
-        let minmax = line.split_whitespace().map(|f| f.parse::<u32>().unwrap()).minmax();
-        if let MinMaxResult::MinMax(min, max) = minmax {
-            sum += max - min;
-        } else {
-            panic!();
-        }
+        let values = line
+            .split_whitespace()
+            .map(|f| f.parse::<u32>().unwrap())
+            .collect_vec();
+        sum += dividable_result(&values);
     }
 
     println!("{sum}");
