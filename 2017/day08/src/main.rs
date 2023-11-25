53293a297c9b3c3d11402fd569c73d675148fd46
Martin Liska <martin.liska@hey.com>
2023-11-25 19:18:40 +0100

    Finish part 1

---
+++ 2017/day08/src/main.rs
@@ -0,0 +1,44 @@
+#[allow(unused)]
+use std::{collections::HashMap, collections::HashSet, fs};
+
+use itertools::Itertools;
+
+#[allow(dead_code)]
+const MOVES: [(i32, i32); 4] = [(0, 1), (1, 0), (0, -1), (-1, 0)];
+
+fn main() {
+    // c dec -10 if a >= 1
+
+    let mut registers = HashMap::new();
+    let data = fs::read_to_string("input.txt").unwrap();
+
+    for line in data.lines() {
+        let (dst, insn, by, _, lhs, op, rhs) = line.split_whitespace().collect_tuple().unwrap();
+        let lhs = *registers.entry(lhs).or_insert(0);
+        let rhs: i32 = rhs.parse().unwrap();
+        let by: i32 = by.parse().unwrap();
+
+        let add = match insn {
+            "inc" => by,
+            "dec" => -by,
+            _ => panic!()
+        };
+
+        let op = match op {
+            "==" => lhs == rhs,
+            "!=" => lhs != rhs,
+            "<" => lhs < rhs,
+            "<=" => lhs <= rhs,
+            ">" => lhs > rhs,
+            ">=" => lhs >= rhs,
+            _ => panic!(),
+        };
+
+        if op {
+            *registers.entry(dst).or_insert(0) += add;
+        }
+    }
+
+    println!("{registers:?}");
+    println!("max={}", registers.values().max().unwrap());
+}
