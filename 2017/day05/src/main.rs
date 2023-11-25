6f023a78b1ede3a322dc1df8c73c31223f2ca76b
Martin Liska <martin.liska@hey.com>
2023-11-25 17:03:13 +0100

    Finish part 1

---
+++ 2017/day05/src/main.rs
@@ -0,0 +1,30 @@
+#[allow(unused)]
+use std::{collections::HashMap, collections::HashSet, fs};
+
+#[allow(dead_code)]
+const MOVES: [(i32, i32); 4] = [(0, 1), (1, 0), (0, -1), (-1, 0)];
+
+fn main() {
+    let mut jumps = Vec::new();
+    for line in fs::read_to_string("input.txt").unwrap().lines() {
+        jumps.push(line.parse::<i32>().unwrap());
+    }
+
+    println!("{jumps:?}");
+    let mut pc: i32 = 0;
+    let mut steps = 0;
+
+    loop {
+        println!("pc={pc}");
+        if pc < 0 || pc as usize >= jumps.len() {
+            println!("exit {pc} steps={steps}");
+            break;
+        }
+        else {
+            let offset = jumps[pc as usize];
+            jumps[pc as usize] += 1;
+            pc += offset;
+            steps += 1;
+        }
+    }
+}
