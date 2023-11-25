176f8fe2a221f15ef958fc9b9cde2709374e854a
Martin Liska <martin.liska@hey.com>
2023-11-25 12:58:32 +0100

    Finish part 2

--- 2017/day01/src/main.rs
+++ 2017/day01/src/main.rs
@@ -5,25 +5,19 @@ use std::{collections::HashMap, collections::HashSet, fs};
 const MOVES: [(i32, i32); 4] = [(0, 1), (1, 0), (0, -1), (-1, 0)];
 
 fn main() {
-    let mut line = fs::read_to_string("input.txt")
+    let line = fs::read_to_string("input.txt")
         .unwrap()
         .lines()
         .next()
         .unwrap()
         .to_string();
-    line.push(line.chars().next().unwrap());
 
     let values: Vec<_> = line.chars().map(|c| c.to_digit(10).unwrap()).collect();
     let s: u32 = values
-        .windows(2)
-        .filter_map(|pair| {
-            if pair[0] == pair[1] {
-                Some(pair[1])
-            } else {
-                None
-            }
-        })
+        .iter()
+        .enumerate()
+        .map(|(i, &v)| (v, values[(i + values.len() / 2) % values.len()]))
+        .filter_map(|pair| if pair.0 == pair.1 { Some(pair.0) } else { None })
         .sum();
-
     println!("{s:?}");
 }
