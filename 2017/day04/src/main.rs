6c15ed9c1dc45b7339f51977b184efdd183ac51c
Martin Liska <martin.liska@hey.com>
2023-11-25 16:55:34 +0100

    Finish part 2

--- 2017/day04/src/main.rs
+++ 2017/day04/src/main.rs
@@ -1,5 +1,6 @@
 #[allow(unused)]
 use std::{collections::HashMap, collections::HashSet, fs};
+use itertools::{sorted, Itertools};
 
 #[allow(dead_code)]
 const MOVES: [(i32, i32); 4] = [(0, 1), (1, 0), (0, -1), (-1, 0)];
@@ -7,8 +8,8 @@ const MOVES: [(i32, i32); 4] = [(0, 1), (1, 0), (0, -1), (-1, 0)];
 fn main() {
     let mut n = 0;
     for line in fs::read_to_string("input.txt").unwrap().lines() {
-        let words: Vec<_> = line.split_whitespace().collect();
-        if words.len() == (HashSet::<&str>::from_iter(words.iter().cloned()).len()) {
+        let words: Vec<_> = line.split_whitespace().map(|s| String::from_iter(s.chars().sorted())).collect();
+        if words.len() == (HashSet::<String>::from_iter(words.iter().cloned()).len()) {
             n += 1;
         }
     }
