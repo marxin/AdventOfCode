ed6e3d8c329b74a3fb441f0b78341d16c28f71a8
Martin Liska <martin.liska@hey.com>
2023-11-27 22:37:10 +0100

    Finish part 2 (very slooow)

--- 2017/day17/src/main.rs
+++ 2017/day17/src/main.rs
@@ -2,16 +2,22 @@
 use std::{collections::HashMap, collections::HashSet, collections::VecDeque, fs};
 
 fn main() {
-    let mut buffer = vec![0];
-    let mut pos = 0;
+    let mut next_values: HashMap<usize, usize> = HashMap::new();
+    next_values.insert(0, 0);
+    let mut pos: usize = 0;
     let step = 301;
 
-    for i in 1..=2017{
-        pos += step;
-        pos = pos % buffer.len();
-        buffer.insert(pos + 1, i);
-        pos += 1;
+    for i in 1..=50000000 {
+        for _ in 0..step {
+            pos = *next_values.get(&pos).unwrap();
+        }
+
+        next_values.insert(i, *next_values.get(&pos).unwrap());
+        *next_values.get_mut(&pos).unwrap() = i;
+        pos = i;
+
+        println!("{i}");
     }
 
-    println!("{}", buffer[pos + 1]);
+    println!("{}", next_values.get(&0).unwrap());
 }
