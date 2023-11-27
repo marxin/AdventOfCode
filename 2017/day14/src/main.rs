f13f8a3926a0e4e3fb893646a1a770759f0ba4fd
Martin Liska <martin.liska@hey.com>
2023-11-27 16:23:48 +0100

    Finish part 2

--- 2017/day14/src/main.rs
+++ 2017/day14/src/main.rs
@@ -40,13 +40,53 @@ fn calculate_hash(s: &str) -> String {
 fn main() {
     let key = "wenycdww";
     let mut count = 0;
+    let mut map = HashMap::new();
 
-    for i in 0..128 {
-        let h = calculate_hash(format!("{key}-{i}").as_ref());
+    for y in 0..128 {
+        let h = calculate_hash(format!("{key}-{y}").as_ref());
         let bits = h.chars().filter(|&c| c == '1').count();
+        for (x, b) in h.chars().enumerate() {
+            if b == '1' {
+                map.insert((x as i32, y), 0);
+            }
+        }
+
         assert_eq!(128, h.len());
         count += bits;
     }
 
     println!("{count}");
+    assert_eq!(count, map.len());
+
+    let mut done = 0;
+    let mut group_id = 1;
+
+    while done != count {
+        let start = map.iter().filter_map(|tuple| if tuple.1 == &0 { Some(*tuple.0) } else { None}).next().unwrap();
+        *map.get_mut(&start).unwrap() = 1;
+
+        let mut queue = VecDeque::from(vec![start]);
+        let mut seen = HashSet::new();
+        seen.insert(start);
+        done += 1;
+
+        while let Some(p) = queue.pop_back() {
+
+            for m in MOVES {
+                let pos2 = (p.0 + m.0, p.1 + m.1);
+                if let Some(v) = map.get_mut(&pos2) {
+                    if !seen.contains(&pos2) {
+                        *v = group_id;
+                        queue.push_back(pos2);
+                        seen.insert(pos2);
+                        done += 1;
+                    }
+                }
+            }
+        }
+
+        group_id += 1;
+    }
+
+    println!("{}", group_id - 1);
 }
