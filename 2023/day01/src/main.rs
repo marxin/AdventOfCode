a169427c2bd758ac48999d1b02b1477e1e7660ee
Martin Liska <martin.liska@hey.com>
2023-12-01 09:36:34 +0100

    Finish part 2

--- 2023/day01/src/main.rs
+++ 2023/day01/src/main.rs
@@ -3,12 +3,44 @@ use std::{collections::HashMap, collections::HashSet, collections::VecDeque, fs}
 
 #[allow(dead_code)]
 const MOVES: [(i32, i32); 4] = [(0, 1), (1, 0), (0, -1), (-1, 0)];
+const DIGITS: [&str; 9] = [
+    "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+];
+
+fn first_digit(line: &str) -> usize {
+    for i in 0..line.len() {
+        let c = line.chars().nth(i).unwrap();
+        if c.is_digit(10) {
+            return c.to_string().parse().unwrap();
+        }
+        for (n, d) in DIGITS.iter().enumerate() {
+            if line[i..].starts_with(d) {
+                return n + 1;
+            }
+        }
+    }
+    panic!()
+}
+
+fn last_digit(line: &str) -> usize {
+    for i in (0..line.len()).rev() {
+        let c = line.chars().nth(i).unwrap();
+        if c.is_digit(10) {
+            return c.to_string().parse().unwrap();
+        }
+        for (n, d) in DIGITS.iter().enumerate() {
+            if line[..=i].ends_with(d) {
+                return n + 1;
+            }
+        }
+    }
+    panic!()
+}
 
 fn main() {
     let mut sum = 0;
     for line in fs::read_to_string("input.txt").unwrap().lines() {
-        let digits: Vec<_> = line.chars().filter(|c| c.is_digit(10)).map(|c| c.to_string().parse::<i32>().unwrap()).collect();
-        let number = 10 * digits.first().unwrap() + digits.last().unwrap();
+        let number = 10 * first_digit(line) + last_digit(line);
         sum += number;
     }
 
