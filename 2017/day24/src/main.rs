14b3dc11dd1039dfd153f240e805c5e2136f519b
Martin Liska <martin.liska@hey.com>
2023-12-01 22:41:37 +0100

    Finish part 1

---
+++ 2017/day24/src/main.rs
@@ -0,0 +1,36 @@
+#[allow(unused)]
+use std::{collections::HashMap, collections::HashSet, collections::VecDeque, fs};
+
+fn find_best(bridges: Vec<(i32, i32)>, last: i32, total: i32, best: &mut i32) {
+    if total > *best {
+        *best = total;
+    }
+
+    for (i, b) in bridges.iter().enumerate() {
+        if b.0 == last || b.1 == last {
+            let next;
+            if b.0 == last {
+                next = b.1;
+            } else {
+                next = b.0;
+            }
+            let mut bridges = bridges.clone();
+            bridges.remove(i);
+            find_best(bridges, next, total + b.0 + b.1, best);
+        }
+    }
+}
+
+fn main() {
+    let mut bridges = Vec::new();
+
+    for line in fs::read_to_string("input.txt").unwrap().lines() {
+        let bridge: Vec<_> = line.split('/').map(|part| part.parse::<i32>().unwrap()).collect();
+        bridges.push((bridge[0], bridge[1]));
+    }
+
+    println!("{bridges:?}");
+    let mut best = 0;
+    find_best(bridges, 0, 0, & mut best);
+    println!("BEST: {best}");
+}
