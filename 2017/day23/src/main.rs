af05b48e1b966de9b2f719982f84c624824fed75
Martin Liska <martin.liska@hey.com>
2023-12-01 21:12:02 +0100

    Part 2 (not done properly)

--- 2017/day23/src/main.rs
+++ 2017/day23/src/main.rs
@@ -12,7 +12,7 @@ impl Pc {
     fn new(insns: Vec<Vec<String>>) -> Self {
         Self {
             insns,
-            regs: HashMap::new(),
+            regs: HashMap::from([("a".to_string(), 1)]),
             pc: 0,
         }
     }
@@ -25,11 +25,53 @@ impl Pc {
         }
     }
 
-    fn run(&mut self) -> u64 {
-        let mut multiplied = 0;
+    fn run(&mut self) -> i64 {
+        let mut prev: Option<HashMap<String, i64>> = None;
         while self.pc >= 0 && (self.pc as usize) < self.insns.len() {
             let insn = &self.insns[self.pc as usize];
-            println!("{:?} {insn:?}", self.regs);
+
+            if self.pc == 23 {
+                let add = 106489;
+                let g = self.regs.get_mut(&"g".to_string()).unwrap();
+                if *g == -add - 1 {
+                    *g += add;
+                    let d = self.regs.get_mut(&"d".to_string()).unwrap();
+                    *d += add;
+                }
+
+                // println!("{} {:?} {insn:?}", self.pc, self.regs);
+            }
+
+            if self.pc == 28 {                
+                print!("{} {:?} {insn:?} // ", self.pc, self.regs);
+                let f = self.regs.get(&"f".to_string()).unwrap();
+                let g = self.regs.get(&"g".to_string()).unwrap();
+                if *f == 1 && *g == -16881 {
+                    let P = 6;
+                    let STEP = 17;
+
+                    for _ in 0..165 {
+                        *self.regs.get_mut(&"d".to_string()).unwrap() += STEP * P;
+                        *self.regs.get_mut(&"e".to_string()).unwrap() += STEP * P;
+                        *self.regs.get_mut(&"g".to_string()).unwrap() += STEP * P;
+                        *self.regs.get_mut(&"b".to_string()).unwrap() += STEP * P;
+                        *self.regs.get_mut(&"h".to_string()).unwrap() += P - 1;
+                    }
+
+                    *self.regs.get_mut(&"h".to_string()).unwrap() += 3;
+                    *self.regs.get_mut(&"g".to_string()).unwrap() += STEP * 3;
+
+                    println!("{} {:?} {insn:?}", self.pc, self.regs);
+                    //panic!();
+                }
+                if let Some(p) = prev {
+                    for (k, v) in self.regs.iter() {
+                        print!("{k}={} ", v - p.get(k).unwrap());
+                    }
+                    println!();
+                }
+                prev = Some(self.regs.clone());
+            }
             let opcode = insn[0].as_ref();
             match opcode {
                 "set" | "sub" | "mul" => {
@@ -39,10 +81,7 @@ impl Pc {
                     match opcode {
                         "set" => *self.regs.get_mut(dst).unwrap() = v,
                         "sub" => *self.regs.get_mut(dst).unwrap() -= v,
-                        "mul" => {
-                            *self.regs.get_mut(dst).unwrap() *= v;
-                            multiplied += 1;
-                        }
+                        "mul" => *self.regs.get_mut(dst).unwrap() *= v,
                         _ => panic!(),
                     }
                 },
@@ -58,11 +97,23 @@ impl Pc {
 
             self.pc += 1;
         }
-        multiplied
+        self.regs[&"h".to_string()]
     }
 }
 
+use primes::is_prime;
+
 fn main() {
+    // it counts non-primes in a range, sigh :()
+    let mut total = 1;
+    for i in (106500..123500).step_by(17) {
+        if !is_prime(i) {
+            total += 1;
+        }
+    }
+
+    println!("{total}");
+
     let mut insns = Vec::new();
 
     for line in fs::read_to_string("input.txt").unwrap().lines() {
@@ -73,6 +124,6 @@ fn main() {
         );
     }
 
-    let multiplied = Pc::new(insns).run();
-    println!("{multiplied}");
+    let h = Pc::new(insns).run();
+    println!("{h}");
 }
