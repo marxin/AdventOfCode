fd911e00714ab3136dfca72dc39a53cf601d47a2
Martin Liska <martin.liska@hey.com>
2023-12-02 09:05:22 +0100

    Finish part 2

--- 2023/day02/src/main.rs
+++ 2023/day02/src/main.rs
@@ -9,22 +9,30 @@ fn main() {
     let maximum = HashMap::from([("red", 12), ("green", 13), ("blue", 14)]);
     let mut good = 0;
     let mut suma = 0;
+    let mut suma2 = 0;
 
     for (i, line) in fs::read_to_string("input.txt").unwrap().lines().enumerate() {
         let parts: Vec<_> = line.split(':').collect();
         let line = parts[1].trim().to_string();
 
         let mut good = true;
+        let mut minimum: HashMap<String, i32> = HashMap::new();
+
         for set in line.split(';') {
             let set = set.replace(",", "");
             for (n, color) in set.split_whitespace().tuples() {
                 let n = n.parse::<i32>().unwrap();
                 let color = color.to_string();
+                minimum.entry(color.to_owned()).and_modify(|x| *x = (*x).max(n)).or_insert(n);
+
                 if n > maximum[&color[..]] {
                     good = false;
                 }
             }
         }
+
+        let mut power = minimum.values().fold(1, |x, y| x * y);
+        suma2 += power;
 
         if good {
             suma += i + 1;
@@ -32,4 +40,5 @@ fn main() {
     }
 
     println!("{suma}");
+    println!("{suma2}");
 }
