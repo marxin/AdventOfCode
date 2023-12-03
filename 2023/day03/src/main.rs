07f488dfe15e25790e472a25aba7b6c4fa5dfb0b
Martin Liska <martin.liska@hey.com>
2023-12-03 22:25:38 +0100

    Finish part 2

--- 2023/day03/src/main.rs
+++ 2023/day03/src/main.rs
@@ -4,6 +4,19 @@ use std::{collections::HashMap, collections::HashSet, collections::VecDeque, fs}
 #[allow(dead_code)]
 const MOVES: [(i32, i32); 4] = [(0, 1), (1, 0), (0, -1), (-1, 0)];
 
+fn are_neighbors(multiply: &(i32, i32), number: &((i32, i32), i32)) -> bool {
+    let number_length = number.1.to_string().len();
+    for x in number.0.0..number.0.0 + (number_length as i32) {
+        let dx = (multiply.0 - x).abs();
+        let dy = (multiply.1 - number.0.1).abs();
+        if dx <= 1 && dy <= 1 {
+            return true;
+        }
+    }
+
+    false
+}
+
 fn main() {   
     let content = fs::read_to_string("input.txt").unwrap();
     let mut map = HashMap::new();
@@ -36,24 +49,47 @@ fn main() {
         }
     }
 
-    const N: i32 = 1000;
-    let mut content = String::new();
-    for y in 0..N {
-        for x in 0..N {
-            let pos = (x, y);
-            if reachable.contains(&pos) {
-                let value = map[&pos];
-                if value.is_digit(10) {
-                    content.push(value);
-                } else {
-                    content.push(',');
-                }
+    let mut numbers = Vec::new();
+
+    // part 2
+    for (y, line) in content.lines().enumerate() {
+        let mut chars: Vec<_> = line.chars().enumerate().collect();
+        let mut x = 0;
+        while x < chars.len() {
+            if chars[x].1.is_digit(10) {
+                let value = String::from_iter(chars[x..].iter().take_while(|c| c.1.is_digit(10)).map(|c| c.1));
+                let number: i32 = value.parse().unwrap();
+                numbers.push(((x as i32, y as i32), number));
+                x += value.len() + 1;
             } else {
-                content.push(',');
+                x += 1;
             }
         }
     }
 
-    let s: i64 = content.split(',').filter(|n| !n.is_empty()).map(|n| n.parse::<i64>().unwrap()).sum();
-    println!("{s}");
+    let mut multiply_map = HashMap::new();
+    for (pos, c) in map.iter() {
+        if *c == '*' {
+            multiply_map.insert(*pos, Vec::new());
+        }
+    }
+
+    for (key, value) in multiply_map.iter_mut() {
+        for number in numbers.iter() {
+            if are_neighbors(key, number) {
+                value.push(number.1);
+            }
+        }
+    }
+
+    let mut total = 0;
+    println!("{multiply_map:?}");
+
+    for value in multiply_map.values() {
+        if value.len() == 2 {
+            total += value[0] * value[1];
+        }
+    }
+
+    println!("{total}");
 }
