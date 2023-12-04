619c2a75953d53d4b385f720910ce3cb2d9ddcef
Martin Liska <martin.liska@hey.com>
2023-12-04 20:39:03 +0100

    Finish part 2

--- 2023/day04/src/main.rs
+++ 2023/day04/src/main.rs
@@ -5,13 +5,33 @@ use std::{collections::HashMap, collections::HashSet, collections::VecDeque, fs}
 const MOVES: [(i32, i32); 4] = [(0, 1), (1, 0), (0, -1), (-1, 0)];
 
 fn main() {
-    let mut sum = 0;
-    for line in fs::read_to_string("input.txt").unwrap().lines() {
+    let mut wins = HashMap::new();
+
+    for (i, line) in fs::read_to_string("input.txt").unwrap().lines().enumerate() {
         let parts: Vec<_> = line.split(':').nth(1).unwrap().split(" | ").collect();
-        let source: HashSet<i32> = HashSet::from_iter(parts[0].split(' ').filter(|x| !x.is_empty()).map(|x| x.parse::<i32>().unwrap()));
-        let target: HashSet<i32> = HashSet::from_iter(parts[1].split(' ').filter(|x| !x.is_empty()).map(|x| x.parse::<i32>().unwrap()));
-        sum += 2i32.pow((source.intersection(&target).count() - 1) as u32)
+        let source: HashSet<i32> = HashSet::from_iter(
+            parts[0]
+                .split(' ')
+                .filter(|x| !x.is_empty())
+                .map(|x| x.parse::<i32>().unwrap()),
+        );
+        let target: HashSet<i32> = HashSet::from_iter(
+            parts[1]
+                .split(' ')
+                .filter(|x| !x.is_empty())
+                .map(|x| x.parse::<i32>().unwrap()),
+        );
+        wins.insert(i, source.intersection(&target).count());
     }
 
-    println!("{sum}");
+    let end = wins.len();
+    let mut counts: HashMap<usize, usize> = HashMap::from_iter((0..end).map(|x| (x, 1usize)));
+    for i in 0..end {
+        let count = counts[&i];
+        for next in 1..wins[&i] + 1 {
+            *counts.get_mut(&(i + next)).unwrap() += count;
+        }
+    }
+
+    println!("{:?}", counts.values().sum::<usize>());
 }
