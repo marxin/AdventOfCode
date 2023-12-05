09a2ebb4212d1ce394dfd7fd656551109d458c21
Martin Liska <martin.liska@hey.com>
2023-12-05 14:51:19 +0100

    Update template

--- main.rs
+++ main.rs
@@ -1,5 +1,6 @@
 #[allow(unused)]
 use std::{collections::HashMap, collections::HashSet, collections::VecDeque, fs};
+use itertools::Itertools;
 
 #[allow(dead_code)]
 const MOVES: [(i32, i32); 4] = [(0, 1), (1, 0), (0, -1), (-1, 0)];
