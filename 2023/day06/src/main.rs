b850ab490ef74ce2b12a383e25dc5ea5f33e9b50
Martin Liska <martin.liska@hey.com>
2023-12-06 07:01:27 +0100

    Finish part 2

--- 2023/day06/src/main.rs
+++ 2023/day06/src/main.rs
@@ -10,24 +10,31 @@ const MOVES: [(i32, i32); 4] = [(0, 1), (1, 0), (0, -1), (-1, 0)];
 fn main() {
     let content = fs::read_to_string("input.txt").unwrap();
     let mut lineit = content.lines();
-    let times = lineit.next().unwrap().split(':').last().unwrap().split_whitespace().collect_vec();
-    let distances = lineit.next().unwrap().split(':').last().unwrap().split_whitespace().collect_vec();
+    let time = lineit
+        .next()
+        .unwrap()
+        .split(':')
+        .last()
+        .unwrap()
+        .replace(' ', "")
+        .parse::<u64>()
+        .unwrap();
+    let distance = lineit
+        .next()
+        .unwrap()
+        .split(':')
+        .last()
+        .unwrap()
+        .replace(' ', "")
+        .parse::<u64>()
+        .unwrap();
 
-    let mut result = 1;
-    for (&time, distance) in times.iter().zip(distances) {
-        let time = time.parse::<i32>().unwrap();
-        let distance = distance.parse::<i32>().unwrap();
-
-        let mut valid = 0;
-        for wait in 1..time {
-            if (time - wait) * wait > distance {
-                valid += 1;
-            }
+    let mut valid = 0;
+    for wait in 1..time {
+        if (time - wait) * wait > distance {
+            valid += 1;
         }
-
-        println!("{valid}");
-        result *= valid;
     }
 
-    println!("{result}");
+    println!("{valid}");
 }
