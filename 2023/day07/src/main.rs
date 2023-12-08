5f6b57aa4623e340baf6dbe0140967d7c7083c9d
Martin Liska <martin.liska@hey.com>
2023-12-08 10:23:58 +0100

    Finish part 1

---
+++ 2023/day07/src/main.rs
@@ -0,0 +1,74 @@
+#[allow(unused)]
+use std::{collections::HashMap, collections::HashSet, collections::VecDeque, fs};
+
+#[allow(unused)]
+use itertools::Itertools;
+
+#[allow(dead_code)]
+const MOVES: [(i32, i32); 4] = [(0, 1), (1, 0), (0, -1), (-1, 0)];
+
+const VALUES: [char; 13] = ['A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2'];
+
+#[derive(Debug, Eq, PartialEq)]
+struct Hand {
+    cards: String,
+    bid: usize,
+}
+
+impl Hand {
+    fn new(cards: String, bid: usize) -> Self {
+        assert_eq!(5, cards.chars().count());
+        Self { cards, bid }
+    }
+
+    fn counts(&self) -> Vec<usize> {
+        let mut freqs = self.cards.chars().sorted().group_by(|x| *x).into_iter().map(|(_, v)| v.count()).filter(|x| *x > 1).collect_vec();
+        freqs.sort();
+        freqs.reverse();
+        freqs        
+    }
+}
+
+impl PartialOrd for Hand {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Ord for Hand {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        let freqs = self.counts();
+        let other_freqs = other.counts();
+
+        if freqs != other_freqs {
+            return freqs.cmp(&other_freqs);
+        }
+
+        for (c1, c2) in self.cards.chars().zip(other.cards.chars()) {
+            let p1 = VALUES.iter().position(|&x| x == c1).unwrap();
+            let p2 = VALUES.iter().position(|&x| x == c2).unwrap();
+            if p1 != p2 {
+                return p2.cmp(&p1);
+            }
+        }
+
+        std::cmp::Ordering::Equal
+    }
+}
+
+fn main() {
+    let content = fs::read_to_string("input.txt").unwrap();
+    let mut hands = Vec::new();
+
+    for line in content.lines() {
+        let parts = line.split_whitespace().collect_vec();
+        let hand = Hand::new(parts[0].to_owned(), parts[1].parse().unwrap());
+        hand.counts();
+        hands.push(hand);
+    }
+
+    hands.sort();
+
+    let suma: usize = hands.iter().enumerate().map(|(i, v)| (i + 1) * v.bid).sum();
+    println!("{suma}");
+}
