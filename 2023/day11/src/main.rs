c8e2304b7058579b093f0097aee4f1babf6107c6
Martin Liska <martin.liska@hey.com>
2023-12-11 10:30:32 +0100

    Clean up part 1

--- 2023/day11/src/main.rs
+++ 2023/day11/src/main.rs
@@ -15,17 +15,23 @@ fn main() {
         lines.push(line.to_string());
     }
 
-    let mut lines2 = lines.into_iter().map(|l| {
-        if l.contains('#') {
-            vec![l]
-        } else {
-            vec![l.to_owned(), l.to_owned()]
-        }
-    }).flatten().collect_vec();
+    let mut lines2 = lines
+        .into_iter()
+        .flat_map(|l| {
+            if l.contains('#') {
+                vec![l]
+            } else {
+                vec![l.to_owned(), l.to_owned()]
+            }
+        })
+        .collect_vec();
 
-    let mut y = lines2.iter().next().unwrap().len() as i32 - 1;
+    let mut y = lines2.first().unwrap().len() as i32 - 1;
     while y >= 0 {
-        if lines2.iter().all(|l| l.chars().nth(y as usize).unwrap() == '.') {
+        if lines2
+            .iter()
+            .all(|l| l.chars().nth(y as usize).unwrap() == '.')
+        {
             for l in lines2.iter_mut() {
                 l.insert(y as usize, '.');
             }
@@ -44,15 +50,20 @@ fn main() {
         }
     }
 
-    let total = positions.iter().cartesian_product(&positions).filter_map(|(a, b)| {
-        if a == b {
-            None
-        } else {
-            Some((a.0 - b.0).abs() + (a.1 - b.1).abs())
-        }
-    }).sum::<i32>();
+    let total = positions
+        .iter()
+        .cartesian_product(&positions)
+        .filter_map(|(a, b)| {
+            if a == b {
+                None
+            } else {
+                Some((a.0 - b.0).abs() + (a.1 - b.1).abs())
+            }
+        })
+        .sum::<i32>();
 
-    println!("{}", lines2.join("\n"));
-    println!("{positions:?}");
+    // println!("{}", lines2.join("\n"));
+    // println!("{positions:?}");
+
     println!("{}", total / 2);
 }
