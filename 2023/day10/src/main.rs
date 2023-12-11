1b82e6ecd3baef6d6323d8f99ea337502f57bd37
Martin Liska <martin.liska@hey.com>
2023-12-11 08:45:46 +0100

    Transform to sub-pixelization

--- 2023/day10/src/main.rs
+++ 2023/day10/src/main.rs
@@ -16,53 +16,49 @@ F is a 90-degree bend connecting south and east.
 #[allow(dead_code)]
 const MOVES: [(i32, i32); 4] = [(0, 1), (1, 0), (0, -1), (-1, 0)];
 
-fn main() {
-    let pipes = [
-        ('|', (0, 1), (0, 1)),
-        ('|', (0, -1), (0, -1)),
-        ('-', (1, 0), (1, 0)),
-        ('-', (-1, 0), (-1, 0)),
-        ('L', (0, 1), (1, 0)),
-        ('L', (-1, 0), (0, -1)),
-        ('J', (0, 1), (-1, 0)),
-        ('J', (1, 0), (0, -1)),
-        ('7', (1, 0), (0, 1)),
-        ('7', (0, -1), (-1, 0)),
-        ('F', (0, -1), (1, 0)),
-        ('F', (-1, 0), (0, 1)),
-    ];
+const PIXEL: i32 = 3;
 
-    let mut map = HashMap::new();
+fn print_map(width: i32, height: i32, map: &HashSet<(i32, i32)>) {
+    for y in 0..PIXEL * height {
+        for x in 0..PIXEL * width {
+            if map.contains(&(x as i32, y as i32)) {
+                print!("#");
+            } else {
+                print!(".");
+            }
+        }
+        println!();
+    }
+    println!();
+}
+
+fn main() {
+    let pixelization = HashMap::from([
+        ('|', HashSet::from([(1, 0), (1, 1), (1, 2)])),
+        ('-', HashSet::from([(0, 1), (1, 1), (2, 1)])),
+        ('L', HashSet::from([(1, 0), (1, 1), (2, 1)])),
+        ('J', HashSet::from([(1, 0), (1, 1), (0, 1)])),
+        ('J', HashSet::from([(0, 1), (1, 1), (1, 0)])),
+        ('7', HashSet::from([(0, 1), (1, 1), (1, 2)])),
+        ('F', HashSet::from([(1, 2), (1, 1), (2, 1)])),
+        ('S', HashSet::from([(1, 0), (0, 1), (1, 1), (2, 1), (1, 2)])),
+    ]);
+
+    let mut map = HashSet::new();
 
     let content = fs::read_to_string("input.txt").unwrap();
+    let width = content.lines().next().unwrap().len() as i32;
+    let height = content.lines().count() as i32;
+
     for (y, line) in content.lines().enumerate() {
-        for(x, c) in line.chars().enumerate() {
-            map.insert((x as i32, y as i32), c);
+        for (x, c) in line.chars().enumerate() {
+            if let Some(pixels) = pixelization.get(&c) {
+                for (px, py) in pixels {
+                    map.insert((PIXEL * x as i32 + px, PIXEL * y as i32 + py));
+                }
+            }
         }
     }
 
-    println!("{map:?}");
-
-    let start = map.iter().find(|(_, &v)| v == 'S').unwrap().0.to_owned();
-    println!("START: {start:?}");
-
-    let mut orientation = (0, 1);
-    let mut pos = start;
-    let mut steps = 0;
-
-    while pos != start || steps == 0 {
-        let nextpos = (pos.0 + orientation.0, pos.1 + orientation.1);
-        if map[&nextpos] == 'S' {
-            break;
-        }
-
-        let next_orient = pipes.iter().find(|x| x.0 == map[&nextpos] && x.1 == orientation).unwrap().2;
-        pos = nextpos;
-        orientation = next_orient;
-        steps += 1;
-        println!("{pos:?} {orientation:?}");
-    }
-    
-    println!("{}", (steps + 1) / 2);
-
+    print_map(width, height, &map);
 }
