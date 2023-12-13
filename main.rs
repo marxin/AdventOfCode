d326aa0b520c3e3bca50f68863ace5a69d01c230
Martin Liska <martin.liska@hey.com>
2023-12-13 07:20:23 +0100

    Update template

--- main.rs
+++ main.rs
@@ -9,7 +9,12 @@ const MOVES: [(i32, i32); 4] = [(0, 1), (1, 0), (0, -1), (-1, 0)];
 
 fn main() {
     let content = fs::read_to_string("input.txt").unwrap();
-    for line in content.lines() {
+    let lines = content.lines().collect_vec();
+
+    let _width = lines.first().unwrap().len() as i32;
+    let _height = lines.len() as i32;
+
+    for (y, line) in lines.iter().enumerate() {
         println!("{line}")
     }
 }
