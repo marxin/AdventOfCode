db8a0330e77f290eb0e0089cd39de25dd4a1e6b8
Martin Liska <martin.liska@hey.com>
2023-12-13 07:15:12 +0100

    Finish part 2

--- 2023/day13/src/main.rs
+++ 2023/day13/src/main.rs
@@ -7,7 +7,9 @@ use itertools::Itertools;
 #[allow(dead_code)]
 const MOVES: [(i32, i32); 4] = [(0, 1), (1, 0), (0, -1), (-1, 0)];
 
-fn reflection_x(map: &HashSet<(i32, i32)>, W: i32) -> Option<i32> {
+fn reflection_x(map: &HashSet<(i32, i32)>, W: i32) -> Vec<i32> {
+    let mut results = Vec::new();
+
     for x in 1..W {
         if map.iter().all(|p| {
                 let tocheck =(x - (p.0 - x) - 1, p.1);
@@ -20,14 +22,16 @@ fn reflection_x(map: &HashSet<(i32, i32)>, W: i32) -> Option<i32> {
                     false
                 }
             }) {
-            return Some(x);
+            results.push(x);
         }
     }
 
-    None
+    results
 }
 
-fn reflection_y(map: &HashSet<(i32, i32)>, H: i32) -> Option<i32> {
+fn reflection_y(map: &HashSet<(i32, i32)>, H: i32) -> Vec<i32> {
+    let mut results = Vec::new();
+
     for y in 1..H {
         let tocheck = y.min(H - y);
         if map.iter().all(|p| {
@@ -41,11 +45,11 @@ fn reflection_y(map: &HashSet<(i32, i32)>, H: i32) -> Option<i32> {
                     false
                 }                
             }) {
-            return Some(y);
+            results.push(y);
         }
     }
 
-    None
+    results
 }
 
 fn main() {
@@ -70,12 +74,47 @@ fn main() {
         }
 
         let rx = reflection_x(&map, W);
+        assert!(rx.len() <= 1);
         let ry = reflection_y(&map, H);
-        if let Some(x) = rx {
-            total += x;
-        } else {
-            total += 100 * ry.unwrap();
+        assert!(ry.len() <= 1);
+        // println!("has: {rx:?} {ry:?}");
+
+        let mut done = false;
+        'outer: for x in 0..W {
+            for y in 0..H {
+                let mut map2 = map.clone();
+                let p = (x, y);
+                if map2.contains(&p) {
+                    map2.remove(&p);
+                } else {
+                    map2.insert(p);
+                }
+
+                let mut rx2 = reflection_x(&map2, W);
+                let mut ry2 = reflection_y(&map2, H);
+                if !rx2.is_empty() && rx != rx2 {
+                    if !rx.is_empty() {
+                        rx2.retain(|v| v != &rx[0]);
+                    }
+                    total += rx2[0];
+                    done = true;
+                    break 'outer;
+                }
+                else if !ry2.is_empty() && ry != ry2 {
+                    if !ry.is_empty() {
+                        ry2.retain(|v| v != &ry[0]);
+                    }
+                    total += 100 * ry2[0];
+                    done = true;
+                    break 'outer;
+                }              
+            }
         }
+
+        assert_eq!(true, done);
+
+
+        
     }
 
     println!("{total}");
