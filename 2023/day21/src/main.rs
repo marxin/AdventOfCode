049a779040096ea30f35a9ace43a1e82b493d9ea
Martin Liska <martin.liska@hey.com>
2023-12-21 23:53:02 +0100

    Finish part 2

--- 2023/day21/src/main.rs
+++ 2023/day21/src/main.rs
@@ -25,38 +25,109 @@ fn step(map: &HashSet<(i32, i32)>, positions: HashSet<(i32, i32)>) -> HashSet<(i
     next
 }
 
+fn print(
+    map: &HashSet<(i32, i32)>,
+    rocks: &HashSet<(i32, i32)>,
+    N: i32,
+    K: i32,
+    start: &(i32, i32),
+) {
+    for yy in -K..=K {
+        for y in 0..N {
+            for xx in -K..=K {
+                for x in 0..N {
+                    let p = (x + xx * N, y + yy * N);
+                    if p == *start {
+                        print!("S");
+                    } else if map.contains(&p) {
+                        print!("O");
+                    } else if rocks.contains(&p) {
+                        print!("#");
+                    } else {
+                        print!(".");
+                    }
+                }
+            }
+            println!();
+        }
+    }
+}
+
 fn main() {
     let content = fs::read_to_string("input.txt").unwrap();
     let lines = content.lines().collect_vec();
 
     let width = lines.first().unwrap().len() as i32;
     let height = lines.len() as i32;
+    let N = width;
+    assert_eq!(width, height);
+    println!("{width}x{height}");
 
     let mut start = None;
     let mut map = HashSet::new();
+    let mut rocks = HashSet::new();
 
     for (y, line) in lines.iter().enumerate() {
         for (x, c) in line.chars().enumerate() {
             let pos = (x as i32, y as i32);
             match c {
-                '#' => {},
+                '#' => {
+                    rocks.insert(pos);
+                }
                 '.' => {
                     map.insert(pos);
-                },
+                }
                 'S' => {
                     start = Some(pos);
                     map.insert(pos);
-                },
-                _ => todo!()
+                }
+                _ => todo!(),
+            }
+        }
+    }
+
+    let mut supermap = HashSet::new();
+    let mut superrock = HashSet::new();
+
+    // copy map
+    let K = 10;
+    for x in -K..K {
+        for y in -K..K {
+            for p in map.iter() {
+                supermap.insert((x * N + p.0, y * N + p.1));
+            }
+        }
+    }
+
+    for x in -K..K {
+        for y in -K..K {
+            for p in rocks.iter() {
+                superrock.insert((x * N + p.0, y * N + p.1));
             }
         }
     }
 
     let mut positions: HashSet<(i32, i32)> = HashSet::from_iter(vec![start.unwrap()]);
+    print(&positions, &superrock, N, 1, &start.unwrap());
+    println!();
 
-    for _ in 0..64 {
-        positions = step(&map, positions);
+    let mut last = 0;
+
+    let mut D = 1u32;
+    for i in 1..1000 {
+        positions = step(&supermap, positions);
+
+        if i >= 65 && (i - 65) % 131 == 0 {
+            // print(&positions, &superrock, N, 1, &start.unwrap());
+            println!(
+                "#{} has {} points, per square: {}",
+                i,
+                positions.len(),
+                positions.len() as f32 / (D.pow(2) as f32)
+            );
+            D += 2;
+        }
     }
 
-    println!("{}", positions.len());
+    //println!("{}", positions.len());
 }
