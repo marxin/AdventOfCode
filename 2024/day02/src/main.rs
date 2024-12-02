82ef036b21b611e167aea2cb85aabfdcdbc3042f
Martin Liska <martin.liska@hey.com>
2024-12-02 16:16:51 +0100

    Finish Aoc 2024 day2 part 2

--- 2024/day02/src/main.rs
+++ 2024/day02/src/main.rs
@@ -10,6 +10,16 @@ struct Point(i32, i32);
 #[allow(dead_code)]
 const MOVES: [Point; 4] = [Point(0, 1), Point(1, 0), Point(0, -1), Point(-1, 0)];
 
+fn is_safe(values: &[i64]) -> bool {
+    let diffs = values
+        .windows(2)
+        .map(|pairs| pairs[0] - pairs[1])
+        .collect_vec();
+
+    diffs.iter().all(|x| x.abs() > 0 && x.abs() <= 3)
+        && diffs.iter().map(|x| x.signum()).all_equal()
+}
+
 fn main() {
     let content = fs::read_to_string("input.txt").unwrap();
     let lines = content.lines().collect_vec();
@@ -20,15 +30,18 @@ fn main() {
             .split_whitespace()
             .map(|x| x.parse::<i64>().unwrap())
             .collect_vec();
-        let diffs = values
-            .windows(2)
-            .map(|pairs| pairs[0] - pairs[1])
-            .collect_vec();
 
-        if diffs.iter().all(|x| x.abs() > 0 && x.abs() <= 3)
-            && diffs.iter().map(|x| x.signum()).all_equal()
-        {
+        if is_safe(&values) {
             count += 1;
+        } else {
+            for i in 0..values.len() {
+                let mut values2 = values.clone();
+                values2.remove(i);
+                if is_safe(&values2) {
+                    count += 1;
+                    break;
+                }
+            }
         }
     }
 
