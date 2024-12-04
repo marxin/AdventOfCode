daca54e26073c7d9057fc2a313ac89804ec6a669
Martin Liska <martin.liska@hey.com>
2024-12-04 08:02:23 +0100

    Finish Aoc 2024 day3 part 2

--- 2024/day03/src/main.rs
+++ 2024/day03/src/main.rs
@@ -13,14 +13,23 @@ const MOVES: [Point; 4] = [Point(0, 1), Point(1, 0), Point(0, -1), Point(-1, 0)]
 
 fn main() {
     let content = fs::read_to_string("input.txt").unwrap();
-    let regex = Regex::new(r"mul\(([0-9]+),([0-9]+)\)").unwrap();
+    let regex = Regex::new(r"mul\(([0-9]+),([0-9]+)\)|don't\(\)|do\(\)").unwrap();
 
-    let s: i64 = regex
-        .captures_iter(&content)
-        .map(|c| {
-            c.get(1).unwrap().as_str().parse::<i64>().unwrap()
-                * c.get(2).unwrap().as_str().parse::<i64>().unwrap()
-        })
-        .sum();
-    dbg!(s);
+    let mut enabled = true;
+    let mut total = 0;
+
+    for capture in regex.captures_iter(&content) {
+        match capture.get(0).unwrap().as_str() {
+            "do()" => enabled = true,
+            "don't()" => enabled = false,
+            _ => {
+                if enabled {
+                    total += capture.get(1).unwrap().as_str().parse::<i64>().unwrap()
+                        * capture.get(2).unwrap().as_str().parse::<i64>().unwrap();
+                }
+            }
+        }
+    }
+
+    dbg!(total);
 }
