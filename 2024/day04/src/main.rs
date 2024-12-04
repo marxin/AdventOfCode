a22a8926a6ffdd949daebbb7ee030dc34e74b22a
Martin Liska <martin.liska@hey.com>
2024-12-04 20:36:15 +0100

    Finish Aoc 2024 day4 part 1

--- 2024/day04/src/main.rs
+++ 2024/day04/src/main.rs
@@ -22,15 +22,40 @@ const MOVES_WITH_DIAGONAL: [Point; 8] = [
     Point(-1, -1),
 ];
 
+const NEEDLE: &str = "XMAS";
+
+fn contains(start: Point, dir: &Point, letters: &HashMap<Point, char>) -> bool {
+    NEEDLE.chars().enumerate().all(|(i, c)| {
+        letters
+            .get(&Point(
+                start.0 + i as i32 * dir.0,
+                start.1 + i as i32 * dir.1,
+            ))
+            .is_some_and(|v| v == &c)
+    })
+}
+
 fn main() {
     let content = fs::read_to_string("input.txt").unwrap();
     let lines = content.lines().collect_vec();
 
-    let _width = lines.first().unwrap().len() as i32;
-    let _height = lines.len() as i32;
+    let width = lines.first().unwrap().len() as i32;
+    let height = lines.len() as i32;
 
-    #[allow(unused)]
+    let mut letters = HashMap::new();
+
     for (y, line) in lines.iter().enumerate() {
-        println!("{line}")
+        for (x, c) in line.chars().enumerate() {
+            letters.insert(Point(x as i32, y as i32), c);
+        }
     }
+
+    let matches = MOVES_WITH_DIAGONAL
+        .iter()
+        .cartesian_product(0..width)
+        .cartesian_product(0..height)
+        .filter(|((dir, x), y)| contains(Point(*x, *y), dir, &letters))
+        .count();
+
+    dbg!(matches);
 }
