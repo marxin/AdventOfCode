fafb0aaddd2d9154bc44efda98899e831d6d66f4
Martin Liska <martin.liska@hey.com>
2024-12-06 17:34:13 +0100

    Finish part 2

--- 2024/day06/src/main.rs
+++ 2024/day06/src/main.rs
@@ -10,6 +10,32 @@ struct Point(i32, i32);
 #[allow(dead_code)]
 const MOVES: [Point; 4] = [Point(0, -1), Point(1, 0), Point(0, 1), Point(-1, 0)];
 
+fn is_looping(mut pos: Point, map: &HashMap<Point, char>, wall: Point) -> bool {
+    let mut direction = 0;
+
+    let mut visited = HashSet::new();
+    loop {
+        let tuple = (pos, direction);
+        if visited.contains(&tuple) {
+            return true;
+        } else {
+            visited.insert(tuple);
+        }
+
+        let step = MOVES[direction];
+        let next = Point(pos.0 + step.0, pos.1 + step.1);
+        let value = map.get(&next);
+        let Some(value) = value else {
+            return false;
+        };
+        if *value == '#' || next == wall {
+            direction = (direction + 1) % MOVES.len();
+        } else {
+            pos = next;
+        }
+    }
+}
+
 fn main() {
     let content = fs::read_to_string("input.txt").unwrap();
     let lines = content.lines().collect_vec();
@@ -30,24 +56,17 @@ fn main() {
         }
     }
 
-    let mut pos = pos.unwrap();
-    let mut direction = 0;
+    let pos = pos.unwrap();
+    let candidates = map
+        .iter()
+        .filter(|&(k, v)| *k != pos && *v == '.')
+        .map(|x| x.0)
+        .collect_vec();
 
-    let mut visited = HashSet::new();
-    loop {
-        visited.insert(pos);
-        let step = MOVES[direction];
-        let next = Point(pos.0 + step.0, pos.1 + step.1);
-        let value = map.get(&next);
-        let Some(value) = value else {
-            break;
-        };
-        if *value == '#' {
-            direction = (direction + 1) % MOVES.len();
-        } else {
-            pos = next;
-        }
-    }
-
-    dbg!(visited.len());
+    dbg!(
+        candidates
+            .iter()
+            .filter(|c| { is_looping(pos, &map, ***c) })
+            .count()
+    );
 }
