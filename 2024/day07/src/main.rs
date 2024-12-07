0511b31c54c8e1d398dae132d6f1525595c148be
Martin Liska <martin.liska@hey.com>
2024-12-07 14:53:38 +0100

    Finish Aoc 2024 day7 part 1

---
+++ 2024/day07/src/main.rs
@@ -0,0 +1,55 @@
+#[allow(unused)]
+use std::{collections::HashMap, collections::HashSet, collections::VecDeque, fs};
+
+#[allow(unused)]
+use itertools::Itertools;
+
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash, Copy)]
+struct Point(i32, i32);
+
+#[allow(dead_code)]
+const MOVES: [Point; 4] = [Point(0, 1), Point(1, 0), Point(0, -1), Point(-1, 0)];
+
+#[allow(dead_code)]
+const MOVES_WITH_DIAGONAL: [Point; 8] = [
+    Point(0, 1),
+    Point(1, 0),
+    Point(0, -1),
+    Point(-1, 0),
+    Point(1, 1),
+    Point(1, -1),
+    Point(-1, 1),
+    Point(-1, -1),
+];
+
+fn can_solve(total: u64, target: u64, values: &[u64]) -> bool {
+    if values.is_empty() {
+        return total == target;
+    }
+
+    if can_solve(total + values[0], target, &values[1..]) {
+        true
+    } else {
+        can_solve(total * values[0], target, &values[1..])
+    }
+}
+
+fn main() {
+    let content = fs::read_to_string("input.txt").unwrap();
+    let lines = content.lines().collect_vec();
+
+    let mut total = 0;
+    for line in lines {
+        let (goal, values) = line.split_once(":").unwrap();
+        let goal = goal.parse().unwrap();
+        let values = values
+            .split_whitespace()
+            .map(|t| t.parse().unwrap())
+            .collect_vec();
+        if can_solve(0, goal, &values) {
+            total += goal;
+        }
+    }
+
+    dbg!(total);
+}
