79633e9169f5000075df4c3a4372a28c7da5f33f
Martin Liska <martin.liska@hey.com>
2024-12-11 14:08:54 +0100

    Finish Aoc 2024 day11 part 2

--- 2024/day11/src/main.rs
+++ 2024/day11/src/main.rs
@@ -1,43 +1,50 @@
-use std::ops::{Add, Sub};
 #[allow(unused)]
 use std::{collections::HashMap, collections::HashSet, collections::VecDeque, fs};
 
 #[allow(unused)]
 use itertools::Itertools;
 
-fn map_numbers(numbers: &[u64]) -> Vec<u64> {
-    numbers
-        .iter()
-        .flat_map(|n| {
-            let digits = n.to_string();
-            let l = digits.len();
-            match (n, l) {
-                (0, _) => vec![1],
-                (_, l) if l % 2 == 0 => {
-                    vec![
-                        digits[..l / 2].parse().unwrap(),
-                        digits[l / 2..].parse().unwrap(),
-                    ]
-                }
-                (n, _) => {
-                    vec![2024 * n]
-                }
-                _ => todo!("unexpected number"),
+fn map_numbers(numbers: &HashMap<u64, usize>) -> HashMap<u64, usize> {
+    let mut output = HashMap::new();
+
+    for (n, times) in numbers.iter() {
+        let digits = n.to_string();
+        let l = digits.len();
+        let values = match (n, l) {
+            (0, _) => vec![1],
+            (_, l) if l % 2 == 0 => {
+                vec![
+                    digits[..l / 2].parse().unwrap(),
+                    digits[l / 2..].parse().unwrap(),
+                ]
             }
-        })
-        .collect_vec()
+            (n, _) => {
+                vec![2024 * n]
+            }
+        };
+
+        for v in values.into_iter() {
+            *output.entry(v).or_default() += times;
+        }
+    }
+
+    output
 }
 
 fn main() {
     let content = fs::read_to_string("input.txt").unwrap();
-    let mut numbers = content
+    let numbers = content
         .trim()
         .split_whitespace()
         .map(|x| x.parse::<u64>().unwrap())
         .collect_vec();
 
-    for _ in 0..25 {
+    assert!(numbers.iter().all_unique());
+    let mut numbers: HashMap<_, _> = numbers.into_iter().map(|n| (n, 1usize)).collect();
+
+    for _ in 0..75 {
         numbers = map_numbers(&numbers);
     }
-    dbg!(numbers.len());
+
+    dbg!(numbers.values().sum::<usize>());
 }
