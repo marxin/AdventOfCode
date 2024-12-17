4517d9007fd405d5f04030743ade59274d5016e1
Martin Liska <martin.liska@hey.com>
2024-12-17 21:57:06 +0100

    Finish Aoc 2024 day17 part 2

--- 2024/day17/src/main.rs
+++ 2024/day17/src/main.rs
@@ -84,34 +84,14 @@ fn flood_fill<T: Clone, F: Fn(&Point, &T, &Point, &T) -> bool>(
     groups
 }
 
-fn main() {
-    let content = fs::read_to_string("input.txt").unwrap();
-    let parts = content.split_once("\n\n").unwrap();
-    let mut regs = parts
-        .0
-        .lines()
-        .map(|line| {
-            line.split_whitespace()
-                .last()
-                .unwrap()
-                .parse::<u64>()
-                .unwrap()
-        })
-        .collect_vec();
-    dbg!(&regs);
-    let program = parts
-        .1
-        .trim()
-        .split_once(": ")
-        .unwrap()
-        .1
-        .split(",")
-        .map(|x| x.parse::<u64>().unwrap())
-        .collect_vec();
-
+fn run(mut regs: Vec<u64>, program: &Vec<u64>) -> Vec<u64> {
     let mut ip = 0;
+    let mut output = Vec::new();
 
+    let mut vals = 0;
+    let mut ticks = 0;
     loop {
+        ticks += 1;
         let Some(&insn) = program.get(ip) else {
             break;
         };
@@ -143,7 +123,11 @@ fn main() {
                 regs[1] ^= regs[2];
             }
             5 => {
-                print!("{},", op_value % 8);
+                output.push(op_value % 8);
+                //if output.len() > program.len() || output[vals] != program[vals] {
+                //break;
+                //}
+                //vals += 1;
             }
             6 => {
                 regs[1] = regs[0] / 2u64.pow(op_value as u32);
@@ -156,6 +140,82 @@ fn main() {
 
         ip += 2;
     }
+    output
+}
+
+fn print_octal(vals: &[u64]) {
+    for v in vals {
+        print!("{v:o}");
+    }
     println!();
-    dbg!(regs);
+}
+
+fn main() {
+    let content = fs::read_to_string("input.txt").unwrap();
+    let parts = content.split_once("\n\n").unwrap();
+    let mut regs = parts
+        .0
+        .lines()
+        .map(|line| {
+            line.split_whitespace()
+                .last()
+                .unwrap()
+                .parse::<u64>()
+                .unwrap()
+        })
+        .collect_vec();
+    dbg!(&regs);
+    let program = parts
+        .1
+        .trim()
+        .split_once(": ")
+        .unwrap()
+        .1
+        .split(",")
+        .map(|x| x.parse::<u64>().unwrap())
+        .collect_vec();
+
+    print!("program: ");
+    print_octal(&program);
+    println!("LEN: {}", program.len());
+
+    // let mut ending = HashSet::new();
+    const N: usize = 16;
+    let prefix = &program[..N];
+    print!("prefix: ");
+    print_octal(prefix);
+    println!();
+
+    let mut ending = HashSet::new();
+
+    /*
+    34424632
+    34724632
+         */
+
+    // R = 107413700225434
+    // bad 107413700323738
+
+    for a in 0..=100_000_000 {
+        let mut regs = regs.clone();
+        let ac = (a << 24) + 0o34724632;
+        regs[0] = ac;
+
+        let output = run(regs, &program);
+        // if output.len() >= N && &output[..N] == prefix {
+        if output == prefix {
+            ending.insert(ac & ((1 << 24) - 1));
+            print!("{ac} ({ac:o}): ");
+            print_octal(&output);
+        }
+    }
+
+    for x in ending.into_iter().sorted() {
+        println!("   {x:o}");
+    }
+
+    println!("program: ");
+    print_octal(&program);
+
+    //println!("{}", program.len());
 }
