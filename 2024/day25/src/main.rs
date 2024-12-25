4eaca07be17ba4bafe4aa6bcb0c34502b7b1f617
Martin Liska <martin.liska@hey.com>
2024-12-25 08:21:32 +0100

    Finish Aoc 2024 day25 part 1

---
+++ 2024/day25/src/main.rs
@@ -0,0 +1,112 @@
+use std::ops::{Add, Mul, Sub};
+#[allow(unused)]
+use std::{collections::HashMap, collections::HashSet, collections::VecDeque, fs};
+
+#[allow(unused)]
+use itertools::Itertools;
+
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash, Copy)]
+struct Point(i64, i64);
+
+impl Add<Point> for Point {
+    type Output = Point;
+
+    fn add(self, rhs: Point) -> Self::Output {
+        Point(self.0 + rhs.0, self.1 + rhs.1)
+    }
+}
+
+impl Sub<Point> for Point {
+    type Output = Point;
+
+    fn sub(self, rhs: Point) -> Self::Output {
+        Point(self.0 - rhs.0, self.1 - rhs.1)
+    }
+}
+
+impl Mul<i64> for Point {
+    type Output = Point;
+
+    fn mul(self, rhs: i64) -> Self::Output {
+        Point(self.0 * rhs, self.1 * rhs)
+    }
+}
+
+#[allow(dead_code)]
+const MOVES: [Point; 4] = [Point(0, 1), Point(1, 0), Point(0, -1), Point(-1, 0)];
+
+#[allow(dead_code)]
+const MOVES_WITH_DIAGONAL: [Point; 8] = [
+    Point(0, 1),
+    Point(1, 0),
+    Point(0, -1),
+    Point(-1, 0),
+    Point(1, 1),
+    Point(1, -1),
+    Point(-1, 1),
+    Point(-1, -1),
+];
+
+#[allow(dead_code)]
+fn flood_fill<T: Clone, F: Fn(&Point, &T, &Point, &T) -> bool>(
+    map: &HashMap<Point, T>,
+    directions: &[Point],
+    predicate: F,
+) -> Vec<HashSet<Point>> {
+    let mut groups = Vec::new();
+    let mut visited = HashSet::new();
+
+    for (point, c) in map.iter() {
+        if visited.contains(point) {
+            continue;
+        }
+        visited.insert(*point);
+
+        let mut group = HashSet::from([*point]);
+        let mut queue = VecDeque::from([*point]);
+
+        while let Some(p) = queue.pop_front() {
+            for m in directions.iter() {
+                let next = p + *m;
+                if let Some(v) = map.get(&next) {
+                    if predicate(&p, c, &next, v) && !visited.contains(&next) {
+                        queue.push_back(next);
+                        group.insert(next);
+                        visited.insert(next);
+                    }
+                }
+            }
+        }
+
+        groups.push(group);
+    }
+
+    groups
+}
+
+const AREA: usize = 5 * 7;
+
+fn parse_key_door(s: &str) -> HashSet<Point> {
+    let mut set = HashSet::new();
+    for (y, line) in s.lines().enumerate() {
+        for (x, c) in line.chars().enumerate() {
+            if c == '#' {
+                set.insert(Point(x as i64, y as i64));
+            }
+        }
+    }
+    set
+}
+
+fn main() {
+    let content = fs::read_to_string("input.txt").unwrap();
+
+    let entries = content.split("\n\n").map(parse_key_door).collect_vec();
+    dbg!(&entries.len());
+    let count = entries
+        .iter()
+        .cartesian_product(&entries)
+        .filter(|(x, y)| x != y && x.intersection(y).count() == 0) // && x.union(y).count() == AREA)
+        .count();
+    dbg!(count / 2);
+}
