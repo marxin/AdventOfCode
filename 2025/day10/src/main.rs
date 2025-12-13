a615381cb5f0e5f2f3b760c565eb9ebc458c86c5
Martin Liska <martin.liska@hey.com>
2025-12-13 11:11:36 +0100

    another attempt

--- 2025/day10/src/main.rs
+++ 2025/day10/src/main.rs
@@ -4,38 +4,82 @@ use std::{collections::HashMap, collections::HashSet, collections::VecDeque, fs}
 #[allow(unused)]
 use itertools::Itertools;
 
-fn find_best(joltage: Vec<i64>, buttons: &Vec<Vec<i64>>, mut pressed: usize) -> Option<usize> {
+fn calculate_combinations(joltage: usize, keyboards: usize) -> usize {
+    if keyboards <= 1 {
+        return keyboards;
+    }
+
+    let top = (keyboards + joltage - 1) as u128;
+    let bottom = (keyboards - 1) as u128;
+
+    ((1..=top).rev().take(bottom as usize).product::<u128>() / (1..=bottom).product::<u128>())
+        as usize
+}
+
+#[test]
+fn test_calculate_combinations() {
+    assert_eq!(calculate_combinations(10, 4), 84);
+}
+
+fn dump_keyboard_frequency(buttons: &[Vec<i64>]) {
+    let fl = buttons.iter().flatten().sorted().collect_vec();
+    for i in 0..10i64 {
+        println!("{i}: {}x", fl.iter().filter(|x| ***x == i).count());
+    }
+}
+
+fn find_best(
+    depth: usize,
+    joltage: Vec<i64>,
+    buttons: &[Vec<i64>],
+    mut pressed: usize,
+) -> Option<usize> {
     if joltage.iter().all(|x| *x == 0) {
         return Some(pressed);
-    } else if joltage.iter().any(|x| *x < 0) {
+    } else if joltage.iter().any(|x| *x < 0) || buttons.is_empty() {
         return None;
     }
 
-    let min_value = *joltage.iter().filter(|&&x| x > 0).min().unwrap();
-    let minimal_key = joltage.iter().position(|&x| x == min_value).unwrap() as i64;
-
-    let buttons_with_min = buttons
+    let joltage_pairs = joltage.iter().enumerate().to_owned().collect_vec();
+    let (combinations, buttons_with_min, min_value, min_index) = joltage_pairs
         .iter()
-        .filter(|&keys| keys.contains(&minimal_key))
-        .cloned()
-        .collect_vec();
+        .filter(|x| *x.1 > 0)
+        .map(|(i, v)| {
+            let buttons_with_min = buttons
+                .iter()
+                .filter(|&keys| keys.contains(&(*i as i64)))
+                .cloned()
+                .collect_vec();
+            (
+                calculate_combinations(**v as usize, buttons_with_min.len()),
+                buttons_with_min,
+                **v,
+                *i,
+            )
+        })
+        .min()
+        .unwrap();
+
     let next_buttons = buttons
         .iter()
         .filter(|&kb| !buttons_with_min.contains(kb))
         .cloned()
         .collect_vec();
 
+    if depth == 0 {
+        dbg!((depth, combinations, min_value, min_index, &joltage));
+        dump_keyboard_frequency(&next_buttons);
+    }
+
     pressed += min_value as usize;
 
     let mut best: Option<usize> = None;
     let combs = combination_k(min_value as _, buttons_with_min.len());
-    // println!(
-    //     "doing {min_value} at index {minimal_key} of {} combs with {} keyboards, next buttons: {}",
-    //     combs.len(),
-    //     buttons_with_min.len(),
-    //     next_buttons.len()
-    // );
+    assert_eq!(combs.len(), combinations);
+
+    let mut counter = 0;
     for combination in combs {
+        counter += 1;
         let mut joltage = joltage.clone();
         for (button, times) in buttons_with_min.iter().zip(combination) {
             for b in button {
@@ -43,8 +87,14 @@ fn find_best(joltage: Vec<i64>, buttons: &Vec<Vec<i64>>, mut pressed: usize) -> 
             }
         }
 
-        if let Some(b) = find_best(joltage, &next_buttons, pressed) {
+        if let Some(b) = find_best(depth + 1, joltage, &next_buttons, pressed) {
             best = Some(best.unwrap_or_default().max(b));
+            dbg!(best);
+        }
+
+        if depth == 0 && (combinations / 400) % counter == 0 {
+            let progress = 100.0 * counter as f32 / combinations as f32;
+            println!("{progress:.0}% ({counter}/{combinations})");
         }
     }
 
@@ -67,14 +117,14 @@ fn combinations(values: Vec<i64>, n: usize, budget: usize, result: &mut Vec<Vec<
 }
 
 fn combination_k(budget: usize, n: usize) -> Vec<Vec<i64>> {
+    // dbg!((budget, n));
     let mut results = Vec::new();
     combinations(Vec::new(), n, budget, &mut results);
     results
 }
 
 fn main() {
-    dbg!(combination_k(50, 5).len());
-
+    // dbg!(combination_k(5, 2));
     let content = fs::read_to_string("input.txt").unwrap();
     let lines = content.lines().collect_vec();
 
@@ -98,7 +148,6 @@ fn main() {
                     .collect_vec()
             })
             .sorted_by_key(|x| x.len())
-            .rev()
             .collect_vec();
 
         let joltage = parts
@@ -112,6 +161,6 @@ fn main() {
             .map(|n| n.parse::<i64>().unwrap())
             .collect_vec();
 
-        println!("best={}", find_best(joltage, &buttons, 0).unwrap());
+        println!("best={}", find_best(0, joltage, &buttons, 0).unwrap());
     }
 }
